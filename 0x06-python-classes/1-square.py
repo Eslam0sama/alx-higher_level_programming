4d52583de4d3b97aa2bb7711a325b35fbf912142
Eslam0sama <eslam.abdulla08@gmail.com>
2024-04-03 19:25:42 -0700

    DONE

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" A module that contain square class"""
+
+
+class Square:
+    """
+    class for Square
+    """
+    
+    def __init__(self, size):
+
+       self.__size = size
