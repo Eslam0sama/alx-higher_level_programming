fb46b2e4a6d468c8a135f40d8ca6efea5a2b5740
Eslam0sama <eslam.abdulla08@gmail.com>
2024-04-08 16:15:51 -0700

    DONE

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This is a module-level documentation string.
+
+This is a module that provides a class of Rectangle and its methods.
+
+"""
+
+
+class Rectangle:
+    """This is class-level documentation string.
+
+This class represents a Rectangle object.
+You can find more information about its attributes and methods below.
+
+"""
+    pass
