b33c30e6e8c2643a931679cdfb3099a15c384d75
Eslam0sama <eslam.abdulla08@gmail.com>
2024-04-08 19:22:09 -0700

    DONE

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -44,7 +44,7 @@ You can find more information about its attributes and methods below.
         """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
@@ -69,7 +69,7 @@ You can find more information about its attributes and methods below.
         """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
