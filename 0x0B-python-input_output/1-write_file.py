9f20bf95d98e60d75cb81bcaa65c835c1c01fd31
Eslam0sama <eslam.abdulla08@gmail.com>
2024-04-17 13:04:27 -0700

    done

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" A module that contain the function -> write_file.
+    for more information about the function read the documentation of the function below.
+"""
+
+
+def write_file(filename="", text=""):
+    """A function that writes a string to a text file (UTF8).
+
+    Args:
+        filename (str): The file to write to.
+        text (str): The text to be written.
+
+    Returns: the number of characters written.
+
+    """
+    with open("test.txt", 'w', encoding='UTF8') as f:
+        f.write(text)
+        
+    return len(text)
